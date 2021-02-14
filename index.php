830ee5fc9275a685a20274eefbe1a0721e252c89
eWargreymon <eddyhb1@hotmail.com>
2021-02-14 02:22:55 +0000

    Retoques de la página principal tras login

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'connect.php';
+
 session_start();
 
 if(!$_SESSION['email']){
@@ -18,7 +20,19 @@ if(!$_SESSION['email']){
 </head>
 <body>
     <h1>INICIO DE SESIÓN CORRECTO</h1>
-    <h2>Has iniciado sesión como: <?php echo $_SESSION['email']; ?></h2>
-    <p>Cierra sesión <a href="logout.php">aquí</a></p>
+    <h2>Has iniciado sesión como:
+    <?php 
+        $email = $_SESSION['email'];
+        
+        $sql = "SELECT * FROM usuarios WHERE correo='$email'";
+        $result = mysqli_query($conn,$sql);
+        
+        $usuario=mysqli_fetch_assoc($result);
+        echo $usuario['nombre'];
+    ?>
+    </h2>
+    <h3>Tu dirección de correo electrónico es: <?php echo $usuario['correo']; ?></h3>   
+    <h3>Tu número de teléfono es: <?php echo $usuario['telefono']; ?></h3>    
+    <p>Cierre sesión <a href="logout.php">aquí</a></p>
 </body>
 </html>
